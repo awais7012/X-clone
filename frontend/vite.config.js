e555769de4e79d751ecf4d3568b87760e4f6c57e
awais7012 <ahmadawaisgithub@gmail.com>
2024-11-24 15:24:37 +0500

    Updated Vite config for production backend URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://x-clone-8.onrender.com", // Your production backend URL
+        target: "https://x-clone-10.onrender.com", // Your production backend URL
         changeOrigin: true,
         secure: true, // Make sure to use secure connections in production
         rewrite: (path) => path.replace(/^\/api/, ""), // Optional: removes "/api" from the request
